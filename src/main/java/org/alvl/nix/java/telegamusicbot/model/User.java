22613d82826936f11a4b9dafc29a0791ea64b0fc
champ1k <egor.bondar.82@gmail.com>
2020-07-08 18:52:37 +0300

    Heroku deploy fix.

--- src/main/java/org/alvl/nix/java/telegamusicbot/model/User.java
+++ src/main/java/org/alvl/nix/java/telegamusicbot/model/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Entity(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
     @Column(name = "chatId")
